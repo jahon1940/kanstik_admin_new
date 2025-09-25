9a102daf3c75767a5c9cc278d965a5cea0c58aac
Abbosbek <abbosbekqodirov2501@gmail.com>
2025-09-26 00:43:19 +0500

    add pages

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import type { NextConfig } from "next";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: false,
   },
 };
 
